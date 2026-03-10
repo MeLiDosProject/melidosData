a0b68f54ac96d84634e84080c0da0f3c69cee296
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2026-03-10 20:22:59 +0100

    added load_data and flatten_data function

---
+++ R/load_data.R
@@ -0,0 +1,121 @@
+#' Title
+#'
+#' @param modality
+#' @param site
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+load_data <- function(
+    modality = c("light_glasses", "light_chest", "light_wrist",
+                 "light_glasses_1minute", "light_chest_1minute", "light_wrist_1minute",
+                 "acceptability", "ase", "chronotype", "demographics",
+                 "evaluation", "health", "leba", "vlsq8",
+                 "currentconditions", "exercisediary", "experiencelog",
+                 "lightexposurediary", "sleepdiaries", "wearlog", "wellbeingdiary"),
+    site = c("all", "BAUA", "FUSPCEU", "IZTECH", "KNUST", "MPI",
+             "RISE", "THUAS", "TUM", "UCR")
+) {
+
+  modality <- match.arg(modality)
+  site <- match.arg(site, several.ok = TRUE)
+  if("all" %in% site) {
+    site <- c("BAUA", "FUSPCEU", "IZTECH", "KNUST", "MPI",
+              "RISE", "THUAS", "TUM", "UCR")
+  }
+
+  if(modality %in% c("light_chest", "light_chest_1minute")) {
+    if("MPI" %in% site) {
+      site <- setdiff(site, "MPI")
+      message("Remove site MPI, as there were no chest-worn devices")
+    }
+  }
+
+  if(modality %in% c("light_chest", "light_wrist", "light_glasses")) {
+      message("These datasets are comparatively large (~50MB per site). Download may take a while.")
+  }
+
+  base_url_pt1 <- "https://raw.githubusercontent.com/MeLiDosProject/"
+  base_url_pt3 <- "/main/data/imported/"
+  base_url_pt4 <-
+    if(modality %in% c("light_glasses", "light_chest", "light_wrist",
+                       "light_glasses_1minute", "light_chest_1minute",
+                       "light_wrist_1minute")) {
+      "light/"
+    } else if (modality %in% c("currentconditions", "exercisediary", "experiencelog",
+                               "lightexposurediary", "sleepdiaries", "wearlog",
+                               "wellbeingdiary")) {
+      "continuous/"
+    } else ""
+  base_url_end <- ".RData"
+
+  url_strings <-
+    site |>
+    rlang::set_names() |>
+    purrr::map_chr(
+      \(x) paste0(base_url_pt1, lookuptable[x],
+                  base_url_pt3, base_url_pt4, modality, base_url_end)
+      )
+
+  if(modality == "sleepdiaries") {
+    modality <- "sleepdiary"
+  }
+  if(stringr::str_detect(modality, "1minute")) {
+    modality <- stringr::str_replace(modality,"1minute", "1min")
+  }
+
+  loaded_data <-
+  url_strings |>
+  purrr::map(
+    \(x) {
+      try(load(url(x)))
+      if(exists(modality)) {
+        get(modality)
+        } else return()
+      },
+    .progress = paste0("loading modality: ", modality)
+  )
+
+  if(length(loaded_data) == 1) return(loaded_data[[1]]) else {
+  class(loaded_data) <- c("melidos_data", class(loaded_data))
+  loaded_data
+  }
+}
+
+lookuptable <- c(BAUA = "BroszioEtAl_Dataset_2025",
+                 FUSPCEU = "BaezaEtAl_Dataset_2025",
+                 IZTECH = "DidikogluEtAl_Dataset_2025",
+                 KNUST = "AkuffoEtAl_Dataset_2025",
+                 MPI = "GuidolinEtAl_Dataset_2025",
+                 RISE = "NilssonTengelinEtAl_Dataset_2026",
+                 THUAS = "AertsEtAl_Dataset_2025",
+                 TUM = "HildenEtAl_Dataset_2025",
+                 UCR = "Sancho-SalasEtAl_Dataset_2025")
+
+#' Title
+#'
+#' @param melidos_data
+#' @param tz
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+flatten_data <- function(melidos_data, tz = "UTC"){
+  stopifnot("Input must be a list dataset loaded with `load_data()`" =
+              inherits(melidos_data, "melidos_data"))
+  melidos_data |>
+    purrr::map(
+      \(x) if(inherits(x, "data.frame")) {
+        x |>
+          dplyr::mutate(
+            dplyr::across(
+              dplyr::where(lubridate::is.POSIXct),
+              \(y) lubridate::force_tz(y, tz)
+            )
+          )
+      } else x
+    ) |>
+    purrr::list_rbind(names_to = "site")
+}
